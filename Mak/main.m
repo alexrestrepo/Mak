5370c42e029e8c8d2e2116e3f61f776f27de4ddf
Alex Restrepo <alexrestrepo@mac.com>
2016-05-13 16:34:39 -0700

    Initial Commit

---
+++ Mak/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mak
+//
+//  Created by Alex Restrepo on 5/13/16.
+//  Copyright © 2016 restrepo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
